fe4606887097bad809d26ab14d704f07d3c2b6c7
Praneet Khandelwal <praneetkhandelwal1996@gmail.com>
2017-04-17 00:06:48 +0530

    Just some small tweaks

--- scripts/course_page.py
+++ scripts/course_page.py
@@ -7,14 +7,18 @@ class CoursePage(Handler):
 	def get(self):
 		course_code = self.request.get("code")
 		course_details = Course.get_details_course(course_code)
-		curr_user = self.cookie_user()
 		sign_in_error = ""
-		if(curr_user == None):
-			sign_in_error = "Please sign in before joinind the course"
 		self.render('course_page.html',course_details = course_details,sign_in_error = sign_in_error)
 	
 	def post(self):
-		self.write("You are enrolled for the course")
+		course_code = self.request.get("code")
+		course_details = Course.get_details_course(course_code)
+		curr_user = self.cookie_user()
+		if(curr_user == None):
+			sign_in_error = "*Please sign in before joining the course"
+			self.render('course_page.html',course_details = course_details,sign_in_error = sign_in_error)
+		else:
+			self.write("You are enrolled for the course")
 
 
 	
