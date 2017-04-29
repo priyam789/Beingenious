3b08617f7ac77784c122b68717706939b2986563
Praneet Khandelwal <praneetkhandelwal1996@gmail.com>
2017-04-30 03:10:27 +0530

    View participants done

--- scripts/participants.py
+++ scripts/participants.py
@@ -16,5 +16,5 @@ class ParticipantsPage(Handler):
 		else:
 			(user_namelist,user_gradelist) = User_Course.get_users_enrolled(course_code)
 			
-		self.render('participants.html',course_code = course_code,course = course_detail,user_namelist = user_namelist,user_gradelist = user_gradelist)
+		self.render('participants.html',course_code = course_code,course = course_detail,user_namelist = zip(user_namelist,user_gradelist),user_gradelist = user_gradelist)
 
