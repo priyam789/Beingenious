6c157e93877e412641ec059c501e4618c8c85667
Praneet Khandelwal <praneetkhandelwal1996@gmail.com>
2017-04-29 16:26:42 +0530

    Discussion done with one problem related to reply

--- scripts/discussion_page.py
+++ scripts/discussion_page.py
@@ -29,12 +29,31 @@ class DiscussionPage(Handler):
 			error = "Query cannot be posted without a title"
 			self.render('discussion.html',course_code = course_code,course = course_details,error=error)
 		else:
-			single_query = self.construct_query(query_title,query_content,user)
+			query_id = len(course_details.discussion)
+			single_query = self.construct_query(query_title,query_content,query_id,user)
 			course_details.discussion.append(single_query)
 			course_details.put()
-			self.render('discussion.html',course_code = course_code,course = course_details)
+			self.render('discussion.html',course_code = course_code,course = course_details,error= error)
+	
+	def post2(self,course_code):
+		reply_content = self.request.get('reply_desc')
+		q_id = self.request.get('reply_form')
+		user = self.cookie_user()
+		course_details = Course.get_details_course(course_code)
+		error = ""
+		if reply_content:
+			reply_id = len(course_details.discussion[int(q_id)]['reply'])
+			single_reply = self.construct_reply(reply_content,reply_id,user)
+			course_details.discussion[int(q_id)]['reply'].append(single_reply)
+			course_details.put()
+			self.render('discussion.html',course_code = course_code,course = course_details,error = error)
 
-	def construct_query(self,title,content,user):
+	def construct_query(self,title,content,qid,user):
 		query = dict()
-		query = {'title':title,'content':content,'user':user.email,'reply':[]}
+		query = {'query_id':qid,'title':title,'content':content,'user':user.email,'reply':[]}
 		return query
+
+	def construct_reply(self,content,r_id,user):
+		reply = dict()
+		reply = {'reply_id':r_id,'content':content,'user':user.email}
+		return reply
